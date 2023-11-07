b30c2f2a4a1db915227c59eeba929802b557ef66
Veronica Hodo <90301991+veronicahodo@users.noreply.github.com>
2023-11-07 04:30:14 -0700

    Create mob.ts

---
+++ mob.ts
@@ -0,0 +1,13 @@
+var mob = {
+  id: 0,
+  instance: 0,
+  name: "Error #9999",
+  type: "construct",
+  stats: {
+    level: 0,
+    hp: 100,
+    hpMax: 100,
+    str: 10,
+    int: 10,
+    dex: 10,
+}
