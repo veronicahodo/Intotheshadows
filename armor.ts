9046c2ce1dfd2049af5e1aaef75d940d3b3f19e3
Veronica Hodo <90301991+veronicahodo@users.noreply.github.com>
2023-11-07 04:52:07 -0700

    Update armor.ts

--- armor.ts
+++ armor.ts
@@ -1,1 +1,16 @@
-v
+var armor = {
+  name: "A gray t-shirt",
+  displayName: "A #!gray;gray#!; t-shirt",
+  description: "A plain gray t-shirt. It doesn't even have a design on it",
+
+  stats: {
+    rating: 0,
+    protections: {
+      fire: 0,
+      physical: 0,
+      digital: 0,
+      electric: 0,
+      poison: 0
+    }
+  }
+}
